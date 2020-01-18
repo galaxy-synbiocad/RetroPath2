1c2e8c9558d81ae1bb49b96fd809c76c5cfe9f0f
mdulac <melchior.dulac@inra.fr>
2020-01-18 23:00:17 +0100

    testing

--- test/test_run.sh
+++ test/test_run.sh
@@ -3,6 +3,6 @@
 #docker run --network host -d -p 8888:8888 --name test_rp2 brsynth/rp2paths-rest
 docker run -d -p 8888:8888 --name test_rp2 brsynth/retropath2-rest
 sleep 10
-python tool_RetroPath2.py -sinkfile input_sink.csv -sourcefile input_source.csv -maxSteps 3 -rulesfile None -topx 100 -dmin 0 -dmax 1000 -mwmax_source 1000 -mwmax_cof 1000 -server_url http://0.0.0.0:8888/REST -scope_csv output_scope.csv -timeout 30
+python3 tool_RetroPath2.py -sinkfile test_input_sink.dat -sourcefile test_input_source.dat -maxSteps 3 -rulesfile 'None' -topx 100 -dmin 0 -dmax 1000 -mwmax_source 1000 -mwmax_cof 1000 -timeout 30 -scope_csv test_out_scope.csv -server_url http://0.0.0.0:8888/REST
 docker kill test_rp2
 docker rm test_rp2
